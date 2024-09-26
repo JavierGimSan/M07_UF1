5ad29fb5069e5d2ff8de870c1b56936f3654a6a5
JavierGimSan <javi.gimsa@gmail.com>
2024-09-26 10:50:24 +0200

    Exercici 9 corregit

--- Activitat2/Exercici9.py
+++ Activitat2/Exercici9.py
@@ -1,15 +1,15 @@
 paraula1 = input("Introdueix una paraula: ")
 paraula2 = input("Introdueix una altra: ")
-paraula1canvi = paraula2[0]
-paraula2canvi = paraula1[0]
+paraula1canvi = paraula2[0] + paraula2[1]
+paraula2canvi = paraula1[0] + paraula1[1]
 
 
-for index in range (1, len(paraula1)):
+for index in range (2, len(paraula1)):
     paraula1canvi = paraula1canvi + paraula1[index]
 
 print(paraula1canvi)
 
-for index in range (1, len(paraula2)):
+for index in range (2, len(paraula2)):
     paraula2canvi = paraula2canvi + paraula2[index]
 
 print(paraula2canvi)
