76f8b3d36285314c57c23982621d8c853ffbb7b4
JavierGimSan <javi.gimsa@gmail.com>
2024-10-07 14:47:06 +0200

    correcció exercici 5

--- activitat3/exercici5.py
+++ activitat3/exercici5.py
@@ -7,17 +7,6 @@ for x in range(0, len(myTupla)-1, 1):
         x = x+1
     else:
         fraseSenseRepetits = fraseSenseRepetits + myTupla[x]
+tuplaSenseRepetits = tuple(fraseSenseRepetits)
         
-
-print("Tupla sense repetits: " + fraseSenseRepetits)
-
-
-
-
-# fraseLlistaSenseEspais = fraseLlista[0].replace(' ', '')
-# fraseLlistaSenseRepetits = []
-# for x in range(1, len(fraseLlistaSenseEspais[0])-1, 1):
-#     if(fraseLlistaSenseEspais[0][x] not in fraseLlistaSenseRepetits):
-#         fraseLlistaSenseRepetits.append(fraseLlistaSenseEspais[0][x])
-
-# print(fraseLlistaSenseRepetits)
+print("Tupla sense repetits: " + str(tuplaSenseRepetits))
