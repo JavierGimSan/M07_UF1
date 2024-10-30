4e8807c50a35f6f053554dc7b1a4288a8a111a64
JavierGimSan <javi.gimsa@gmail.com>
2024-10-30 20:58:48 +0100

    Creació de taula

--- ACTIVITAT_7/conn.py
+++ ACTIVITAT_7/conn.py
@@ -9,5 +9,16 @@ conn = psycopg2.connect(
 )
 
 connection = conn.cursor()
-
 print(connection)
+
+sql = '''CREATE TABLE USERS(
+                user_id SERIAL PRIMARY KEY,
+                user_name VARCHAR(255) NOT NULL,
+                user_surname VARCHAR(255) NOT NULL,
+                user_age BIGINT NOT NULL,
+                user_email VARCHAR(255) NOT NULL
+)'''
+
+connection.execute(sql)
+conn.commit()
+
