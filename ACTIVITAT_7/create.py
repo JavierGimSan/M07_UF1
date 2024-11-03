5f242bbfbc43988ab6ec51d097119568f77299a4
JavierGimSan <javi.gimsa@gmail.com>
2024-11-03 20:44:29 +0100

    Arxiu 'create.py'

--- ACTIVITAT_7/create.py
+++ ACTIVITAT_7/create.py
@@ -1,1 +1,20 @@
+import psycopg2
 
+conn = psycopg2.connect(
+    database="postgres",
+    user='javier_gimenez',
+    password='javi123',
+    host='localhost',
+    port='5432'
+)
+
+connection = conn.cursor()
+
+def crear_usuari(name, surname, age, email):
+    sql = '''INSERT INTO USERS (
+                    user_name,
+                    user_surname,
+                    user_age,
+                    user_email
+    ) VALUES (?, ?, ?, ?)'''
+    connection.execute(sql, (name, surname, age, email))
